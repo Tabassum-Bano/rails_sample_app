77cb3700980a3e56e774b9aa3ab2ae3ed1b08fea
Tabassum Bano <tabassum.bano@payu.in>
2017-12-31 21:53:22 +0530

    advanced login complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-
   	user=User.find_by(email: params[:session][:email])
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+  		params[:session][:remember_me]== '1' ? remember(user) : forget(user)
   		redirect_to current_user
   		#user profile 
   	else
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
