75066607e338d9dd6756504c590c656d7469bd02
Tabassum Bano <tabassum.bano@payu.in>
2018-01-02 13:12:58 +0530

    account activation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+
+	before_create :create_activation_digest
 	before_save {email.downcase!}
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -28,14 +30,35 @@ class User < ApplicationRecord
 	end
 
 	#returns true if the token matches the digest
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = self.send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	#forgets user by removing remember digest
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	private
+
+	#activation token addition to object
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
+
+	#activates an account
+	def activate
+		update_columns(activated: true, activated_at: Time.zone.now)
+		#update_attribute(:activated, true)
+		#update_attribute(:activated_at, Time.zone.now)
+	end
+
+	#sends activation email
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
 	end
 
 end
